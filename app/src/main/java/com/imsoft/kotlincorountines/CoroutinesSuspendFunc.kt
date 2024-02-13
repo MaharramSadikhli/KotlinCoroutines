e474010c76f990b1de290b3264e5bb206cc458f1
Muarem <s.muarem10@gmail.com>
2024-02-14 03:50:01 +0400

    coroutine suspend functions

---
+++ app/src/main/java/com/imsoft/kotlincorountines/CoroutinesSuspendFunc.kt
@@ -0,0 +1,26 @@
+package com.imsoft.kotlincorountines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    /*
+    runBlocking {
+        delay(2000)
+        println("run blocking")
+        suspendFunc()
+    }
+
+    suspend fun suspendFunc() {
+        coroutineScope {
+            delay(4000)
+            println("coroutine scope at suspend func")
+        }
+    }
+     */
+
+}
+
