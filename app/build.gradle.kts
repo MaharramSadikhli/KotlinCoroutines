7f6d0166dfa55c6f1563f8144bfc04ccf2dbcd12
Muarem <s.muarem10@gmail.com>
2024-02-14 05:51:32 +0400

    implementation coroutines lifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0-RC2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 }
